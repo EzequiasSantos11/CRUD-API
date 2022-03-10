7e9d238d9152d99ebaef35764c05781fe7cfafcb
ezequiassantos11 <ezequiasmg16@gmail.com>
2022-03-10 13:41:19 -0300

    first commit

---
+++ src/Services/UpdateDosService.ts
@@ -0,0 +1,30 @@
+import prismaClient from "../prisma";
+import { response } from "express";
+
+class UpDateDosService{
+  async execute( id, title: string, description: string, deadline: string){
+
+    let user: any = prismaClient.tarefas.findFirst({
+      where:{
+        id: id,
+      }
+    })
+    if(!user){
+     return response.status(401).json({message: "TODO does not found!"})
+    }else{
+      user = await prismaClient.tarefas.update({
+        where:{
+          id: id,
+        },
+        data:{
+          title: title,
+          description: description,
+          deadline: deadline
+        }
+      })
+      return response.status(200).json({message: "sucess"})
+    }
+  }
+}
+
+export {UpDateDosService};
